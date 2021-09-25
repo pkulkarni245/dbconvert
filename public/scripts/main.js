cee9a5f3fc1b4132e8aa44c106d4b3f18e0a4b91
pkulkarni245 <72600670+pkulkarni245@users.noreply.github.com>
2021-09-25 11:48:56 +0530

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,52 +1,31 @@
 $(document).ready(function () {
     localStorage.clear();
     $("#file-upload").change(function(e){
-        var inFiles = e.target.files;
-        localStorage.inFiles = JSON.stringify(inFiles);
-        alert("hererere: " + inFiles[0].name);
+        var files = e.target.files;
+        var fileArray = [];
+        //var file = {};
+        for(var i = 0; i < files.length; i++){
+            //files[i].text().then(t => alert(t));
+            /*file = {
+                'name'  : files[i].name,
+                'size'  : files[i].size,
+                'type'  : files[i].type,
+            }*/
+            fileArray.push(files[i]);
+        }
+        localStorage.setItem('files', JSON.stringify(fileArray));
     });
     $("#upload-form").submit(function(evt){
         evt.preventDefault();
-        var files = JSON.parse(localStorage.inFiles);
-        alert(files);
-        var file;
-        for(var i = 0; i < files.length; i++){
-            file = files[i];
+        var subFiles = JSON.parse(localStorage.files);
+        subFiles[0].text().then(t => alert(t));
+        /*for(var i = 0; i < subFiles.length; i++){
+            alert(subFiles[i].name);
             var reader = new FileReader();
-            reader.readAsText(file);
-            reader.onload = function (e) {
-                console.log(e.target.result);
-            };
-        }
+            reader.readAsText(subFiles[i]);
+            reader.onload = function(e){
+                alert(e.target.result);
+            }
+        }*/
     });
 });
-/*$(document).ready(function () {
-    localStorage.clear();
-    $("#file-upload").change(function(e){
-        var inFiles = e.target.files;
-        localStorage.inFiles = inFiles;
-        var reader = new FileReader();
-        var file = inFiles[0];
-        reader.readAsText(file);
-        reader.onload = function (e) {
-            localStorage.firstFile = e.target.result;
-            alert("ff: " + localStorage.firstFile);
-        };
-        localStorage.jsFile = JSON.stringify(inFiles);
-        console.log("ttt: "+typeof JSON.parse(localStorage.jsFile));
-    });
-    $("#upload-form").submit(function(evt){
-        evt.preventDefault();
-        var files = localStorage.inFiles;
-        var file;
-        for(var i = 0; i < files.length; i++){
-            alert(files);
-            file = files[i];
-            var reader = new FileReader();
-            reader.readAsText(file);
-            reader.onload = function (e) {
-                alert(e.target.result);
-            };
-        }
-    });
-});*/
