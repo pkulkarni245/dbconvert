4091caf1e03cce27f04d799cfea74c046de21b21
pkulkarni245 <72600670+pkulkarni245@users.noreply.github.com>
2021-09-30 18:50:32 +0530

    Obtain Keys and Values

--- public/scripts/process.js
+++ public/scripts/process.js
@@ -1,4 +1,5 @@
 function process(filename, filesize, file) {
+    log("Processing " + filename + "(" + filesize + "B)");
     try {
         if (IsValidJSON(file));
         else {
@@ -8,44 +9,28 @@ function process(filename, filesize, file) {
         invalidJSONError(filename);
     }
     var f = JSON.parse(file);
-    console.log(f);
     for(var i in f){
         var g = f[i];
-        console.log(g);
-        for(var j in g){
-            var h = g[j];
-            console.log(Object.keys(h));
-            console.log(Object.values(h));
-            console.log(IsValidJSON(JSON.parse(g)));
+        //console.log(i + "--->\n" + Object.keys(g) + ": " + Object.values(g));
+        console.log(i + "--->")
+        for(idx in Object.keys(g)){
+            var key = Object.keys(g)[idx];
+            var val = Object.values(g)[idx];
+            if(val == "[object Object]")
+                val = JSON.stringify(val);
+            console.log(key + ": " + val);
+
         }
+        /* For one extra depth level
+        for(j in g){
+            var h = g[j];
+            console.log("-----" + j + "-----");
+            console.log(Object.keys(h) + ": " + Object.values(h));
+        }/*/
     }
-    /*for(var i in f){
-        var g = f[i];
-        console.log(g);
-        for(var j in g){
-            var h = g[j];
-            console.log(h);
-            for(var k in h){
-                console.log(k);
-            }
-        }
-    }*/
-
-    //https://stackoverflow.com/questions/14528385/how-to-convert-json-object-to-javascript-array
-
-    var g = Object.values(f)[0];
-    var h = Object.values(g)[0];
-    var i = Object.values(h)[0];
-    var j = Object.values(i)[0];
-    var k = Object.values(j)[1];
-    console.log(Object.values(k));
-
-    console.log(json2array(f));
-
 }
 
 function batch_process(no_files, filenames, filesizes, files) {
-    log("Processing...");
     for (var i = 0; i < files.length; i++)
         process(filenames[i], filesizes[i], files[i]);
 }
